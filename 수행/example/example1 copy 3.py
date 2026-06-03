f478bee72543d25b9103898e0cf2451a21ff620e
psg0104 <lbkpbc0104@gmail.com>
2026-06-03 19:05:07 +0900

    Remove unused JavaScript, CSS, images, and Python scripts related to triangle area, multiples of three, arithmetic operations, and student scores.

--- 수행/example/example1 copy 3.py
+++
@@ -1,8 +0,0 @@
-'''
-두 정수 a, b가 주어진다.
-두 수의 합, 차, 곱을 각각 출력하시오.
-'''
-a,b = map(int,input().split())
-print(a+b)
-print(a-b)
-print(a*b)
--- 수행/코드만/1번.py
+++
@@ -1,4 +0,0 @@
-le = int(input("삼각형의 밑변 : "))
-hi = int(input("삼각형의 높이 : "))
-
-print("삼각형의 넓이 : %.2f" %(le*hi/2))
